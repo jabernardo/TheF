5a7f2c9e8156e5091b8ab0bab5e3e25b02c0c692
John Aldrich Bernardo <jabernardo@jabernardo-lion.perfectfitgroup.local>
2020-02-17 13:38:45 +0800

    Add some docstring

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ import sys
 import re
 from difflib import SequenceMatcher
 
+##
+# The F!
+# 
+# Shorthand for terminal
+#
+######
+# Installation
+#
+#########
+### Bash
+#
 # function q()
 # {
-#     source="python /home/jabernardo/Workspace/q/main.py"
-
+#     source="python /{installation path}/main.py"
+#
 #     if [[ ( $# -eq 0 ) ]]; then
 #         history -w
 #         $($source $1)
@@ -21,20 +32,47 @@ from difflib import SequenceMatcher
 #         cd $($source $1)
 #     fi
 # }
+#########
+### Fish
+# function f
+#         set -l source "python /{installation path}/main.py"
+#
+#         if test (count $argv) -eq 0
+#                 history save
+#                 eval (eval $source $argv --history "~/.local/share/fish/fish_history")
+#         else
+#                 cd (eval $source $argv)
+#         end
+# end
+#
+#
 
 class TheF:
+    """TheF!
+    An application inspired by Fuck and Jump
+    """
+
+    # User home directory
     homedir = "/"
 
+    # Loaded Configurations
     config = None
 
+    # Configuration filename
     config_file = ".q"
+    # Configuration full path
     config_path = ""
 
+    # Loaded paths from configuration
     paths = None
 
+    # Application Full Path
     application_path = ""
 
     def __init__(self):
+        """TheF"""
+
+        # Apply full home directory
         self.homedir = str(Path.home())
         
         self.config = configparser.ConfigParser()
@@ -44,6 +82,7 @@ class TheF:
             self.config.read(self.config_path)
         
         if not "paths" in self.config:
+            # Make sure path section exists
             self.config["paths"] = {}
 
             self.__save_config()
@@ -53,18 +92,28 @@ class TheF:
         self.application_path = str(Path(__file__).parent)
 
     def __save_config(self):
+        """Save configurations file"""
         with open(self.config_path, "w") as configfile:
             self.config.write(configfile)
 
     def __get_match(self, needle, haystack):
+        """Get closest match for command. Would append parameters or arguments for closest match\n
+
+        \nArguments:\n
+        `needle` (str) -- Needle or the mistaken command\n
+        `haystack` (str) -- Haystack\n
+
+        \nReturns:\n
+        `str|None` Match
+        """
         highest_word = ""
         highest_ratio = 0
 
-        for cmd in haystack:
-            ratio = SequenceMatcher(None, needle, cmd).ratio()
+        for hay in haystack:
+            ratio = SequenceMatcher(None, needle, hay).ratio()
 
             if ratio > highest_ratio:
-                highest_word = cmd
+                highest_word = hay
                 highest_ratio = ratio
         
             params = needle.strip().split(" ")[ len(highest_word.split(" ")): ]
@@ -75,30 +124,68 @@ class TheF:
         return None
 
     ##
-    ## Paths
-    ##
+    ## Jump Path!
+    ###############################################################
 
     def add_path(self, alias, path, force=False):
+        """Add path to Jump list\n
+
+        \nArguments:\n
+        `alias` (str) -- Alias for path\n
+        `path` (str) -- Full path\n
+
+        \nReturns:\n
+        `None`
+        """
         if not alias in self.paths or force:
             self.paths[alias] = path
             self.__save_config()
 
     def get_path(self, alias):
+        """Get path\n
+        \nArguments:\n
+        `alias` (str) -- Path alias\n
+
+        \nReturns:\n
+        `str` Path from alias else current working directory
+        """
         if alias in self.paths:
             return self.paths[alias]
         
         return os.getcwd()
 
     def remove_path(self, alias):
+        """Remove path\n
+        \nArguments:\n
+        `alias` (str) -- Path alias to be removed\n
+        \nReturns:\n
+        `None`
+        """
         if alias in self.paths:
             del self.paths[alias]
             self.__save_config()
 
-    ###############################################################3
-
-    # F*ck!
+    ##
+    ## What the F!
+    ###############################################################
 
     def get_last_command(self, custom_history = "~/.bash_history"):
+        """Get last command from history\n
+
+        \nArguments:\n
+        `custom_history` (str) -- History file. Default history file is for bash\n
+
+        \nReturns:\n
+        `str`\n
+
+        \nNotes:\n
+        - Make sure to update history file before using this command. See installation section for
+        more information\n
+
+        \nSupported Shells:\n
+        - Bash
+        - Fish
+        """
         history = os.path.expanduser(custom_history)
 
         try:
@@ -114,6 +201,22 @@ class TheF:
         return ""
 
     def get_history(self, custom_history = "~/.bash_history"):
+        """Get all history\n
+
+        \nArguments:\n
+        `custom_history` (str) -- History file. Default history file is for bash\n
+
+        \nReturns:\n
+        `list`\n
+
+        \nNotes:\n
+        - Make sure to update history file before using this command. See installation section for
+        more information\n
+
+        \nSupported Shells:\n
+        - Bash
+        - Fish
+        """
         history = os.path.expanduser(custom_history)
 
         try:
@@ -128,6 +231,15 @@ class TheF:
         return []
 
     def get_prediction(self, command, custom_history = "~/.bash_history"):
+        """Get prediction from command\n
+
+        \nArguments:\n
+        `command` (str) Wrong command\n
+        `custom_history` (str) -- History file. Default history file is for bash
+
+        \nReturns:\n
+        `str`
+        """
         commands = []
         
         with open(f"{self.application_path}/commands.txt", "r") as file:
@@ -162,13 +274,17 @@ def main():
         name = os.path.basename(cwd)
 
     if args.add:
+        # Add path to paths list
         q.add_path(name, cwd, args.force)
     elif args.remove:
+        # Remove path from list
         q.remove_path(name)
     else:
         if args.alias:
+            # Directory Jump!
             print(q.get_path(args.alias))
         else:
+            # The F!
             print(q.get_prediction(q.get_last_command(args.history), args.history))
 
 if __name__ == "__main__":
